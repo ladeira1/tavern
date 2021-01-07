f3ddde4657d8954ab60a12a09f69122210f70d69
Victor Ladeira <victorladeirag@gmail.com>
2021-01-06 21:32:44 -0300

    create header menu for mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,38 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Wrapper, Empty, Title, Buttons } from './styles';
+import {
+  Container,
+  Wrapper,
+  Empty,
+  Title,
+  Buttons,
+  Menu,
+  Settings,
+  ButtonArea,
+} from './styles';
 import { useAuth } from '../../contexts/Auth';
 
 const Header: React.FC = () => {
   const { isLogged, logout } = useAuth();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   const handleLogout = () => {
     logout();
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Container>
+    <Container logged={isLogged}>
       <Wrapper>
         <Empty />
         <Title>TAVERN</Title>
-        <Buttons logged={isLogged}>
+        <Buttons>
           <Link to="/user">Profile</Link>
           <Link to="/item">Create Item</Link>
           {isLogged ? (
@@ -30,6 +45,29 @@ const Header: React.FC = () => {
             </Link>
           )}
         </Buttons>
+        <Menu onClick={toggleMenu} isClicked={isOpen}>
+          <div />
+          <div />
+          <div />
+        </Menu>
+
+        {isOpen && (
+          <Settings isClicked={isOpen}>
+            <ButtonArea isLogged={isLogged}>
+              <Link to="/profile">Edit Profile</Link>
+              <Link to="/cart">Cart</Link>
+              {isLogged ? (
+                <button type="button" onClick={handleLogout} className="auth">
+                  Logout
+                </button>
+              ) : (
+                <Link to="/login" className="auth">
+                  login
+                </Link>
+              )}
+            </ButtonArea>
+          </Settings>
+        )}
       </Wrapper>
     </Container>
   );
--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import colors from '../../assets/colors';
 
-export const Container = styled.main`
+export const Container = styled.main<{ logged: boolean }>`
   width: 100vw;
   height: 8vh;
   min-height: 70px;
@@ -9,6 +9,16 @@ export const Container = styled.main`
   overflow: hidden;
   position: static;
   z-index: 10;
+
+  .auth {
+    color: ${props => (props.logged ? colors.red : colors.green)};
+    font-family: 'Ubuntu';
+    font-size: 1.6em;
+
+    :hover {
+      cursor: pointer;
+    }
+  }
 `;
 
 export const Wrapper = styled.article`
@@ -51,7 +61,7 @@ export const Title = styled.h1`
   color: ${colors.text};
 `;
 
-export const Buttons = styled.nav<{ logged: boolean }>`
+export const Buttons = styled.nav`
   flex: 1;
   display: flex;
   flex-direction: row;
@@ -70,16 +80,106 @@ export const Buttons = styled.nav<{ logged: boolean }>`
     @media screen and (max-width: 400px) {
       margin-right: 1em;
     }
+
+    @media screen and (max-width: 810px) {
+      display: none;
+    }
   }
 
   .auth {
-    color: ${props => (props.logged ? colors.red : colors.green)};
-    font-family: 'Ubuntu';
-    font-size: 1.6em;
-    width: ${props => (props.logged ? '19%' : 'auto')};
-
-    :hover {
-      cursor: pointer;
+    @media screen and (max-width: 810px) {
+      display: none;
     }
+  }
+`;
+
+export const Menu = styled.nav<{ isClicked: boolean }>`
+  display: none;
+  z-index: 99;
+  width: 25px;
+  max-width: 25px;
+
+  margin-right: 10px;
+
+  cursor: pointer;
+
+  div {
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+
+    background: ${colors.text};
+    height: 2px;
+    width: 100%;
+    margin: 6px auto;
+  }
+
+  div:first-child {
+    transform: ${props => (props.isClicked ? 'rotate(45deg)' : 'rotate(0)')};
+  }
+
+  div:nth-child(2) {
+    opacity: ${props => (props.isClicked ? '0' : '1')};
+    transform: translateX(${props => (props.isClicked ? '20px' : '0')});
+  }
+
+  div:nth-child(3) {
+    transform: ${props => (props.isClicked ? 'rotate(-45deg)' : 'rotate(0)')};
+  }
+
+  @media screen and (max-width: 810px) {
+    display: block;
+  }
+`;
+export const Settings = styled.div<{ isClicked: boolean }>`
+  transform: translate(${props => (props.isClicked ? '0' : '100%')});
+
+  position: ${props => (props.isClicked ? 'absolute' : 'relative')};
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 80%;
+  max-width: 300px;
+
+  background: ${colors.background};
+  opacity: 0.7;
+  transition: all linear 0.2s;
+
+  z-index: 90;
+`;
+export const ButtonArea = styled.div<{ isLogged: boolean }>`
+  margin-right: 10px;
+  margin-top: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  font-size: 1.2em;
+  font-family: 'Oswald', sans-serif;
+  color: ${colors.text};
+  padding-left: 10px;
+
+  padding-right: 10px;
+
+  a {
+    background-color: ${colors.primary};
+    color: ${colors.text};
+    margin-top: 4%;
+    text-decoration: none;
+    font-size: 2em;
+  }
+
+  .auth {
+    background-color: ${colors.primary};
+    margin-top: 4%;
+    text-decoration: none;
+    font-size: 2em;
+  }
+
+  span {
+    font-size: 1rem;
+    margin-top: 10px;
+    color: ${colors.white};
   }
 `;
